48e8556ecdda2cf3a01f15a89dfab667648f05f9
“Juan <ramirezjuanj0117@gmail.com>
2023-10-03 16:33:05 -0500

    doing footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
 
-export const Footer = () => {
+
+export const Footer = () =>
+{
   return (
-    <div>Footer</div>
+    <>
+      <div className=" w-full bg-slate-800 text-white p-2 pt-6 pl-20">
+          <ul className="flex flex-row text-sm   ">
+            <li className="p-2 m-2">Home</li>
+            <li className="p-2 m-2">|</li>
+            <li className="p-2 m-2">Terms and Conditions</li>
+            <li className="p-2 m-2">|</li>
+            <li className="p-2 m-2">Privacy Policy</li>
+            <li className="p-2 m-2">|</li>
+            <li className="p-2 m-2">Collection Statement</li>
+            <li className="p-2 m-2">|</li>
+            <li className="p-2 m-2">Help</li>
+            <li className="p-2 m-2">|</li>
+            <li className="p-2 m-2">Manage Account</li>
+          </ul>
+      </div>
+    </>
   )
 }
